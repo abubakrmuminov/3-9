7583018f7fec5ee3477281387c51565d631f5914
Abubakr <170409v@gmail.com>
2025-04-17 16:55:41 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,164 @@
+// ================================================================================================================ //
+// ==================================================== 1-QISM ==================================================== //
+// ================================================================================================================ //
+
+
+// ======= //
+// 1-mashq //
+// ======= //
+
+// const key = prompt("kimsiz");
+
+// const user = {
+//     ali: {
+//         name: "Ali",
+//         age: 25
+//     }
+// };
+
+// let res = `Foydalanuvchining ismi: ${user.ali.name} yoshi: ${user.ali.age}`;
+
+// console.log(res);
+
+// ======= //
+// 2-mashq //
+// ======= //
+
+// const product = {
+//     name:"telefon",
+//     price: 1000
+// }
+
+// console.log(product)
+
+// product.productWithTax = product.price*1.12
+
+// console.log(product)
+
+// ======= //
+// 3-mashq //
+// ======= //
+
+// const carInfo = {
+//     model: "cobalt",
+//     year: 2022,
+//     color: "oq"
+// }
+
+// let car = `Bu ${carInfo.color} rangli ${carInfo.model}, ${carInfo.year}-yil`
+
+// console.log(car)
+
+// ======= //
+// 4-mashq //
+// ======= //
+
+// const user = {
+//     name: "ali",
+//     email: "ali@gmail.com"
+// }
+
+// console.log = ("email" in user)
+
+// ======= //
+// 5-mashq //
+// ======= //
+
+// const employees = {
+//   ali: {
+//     name: "ali",
+//     salary: 1000
+//   },
+//   vali: {
+//     name: "vali",
+//     salary: 1500
+//   }
+// };
+
+// let TotalSalary = (employees.ali.salary + employees.vali.salary)
+
+// console.log(TotalSalary)
+
+// ======= //
+// 6-mashq //
+// ======= //
+
+// const car = {
+//   model: "cobalt",
+//   distance: 150,
+//   time: 3,
+// };
+
+// function speed(distance, time) {
+//   car.calculateSpeed = distance * time;
+// }
+
+// speed(car.distance, car.time);
+
+// console.log(car);
+
+
+
+// ======= //
+// 7-mashq //
+// ======= //
+
+// const worker = {
+//     name: "ali",
+//     start: 9,
+//     end:18
+// }
+
+// function time (a, b) {
+//     worker.WorkTime = a - b
+// }
+
+// time(worker.end, worker.start)
+
+// console.log(worker)
+
+
+// ======= //
+// 8-mashq //
+// ======= //
+
+// const cube = {
+//     side: 4
+// }
+
+// cube.volume = (Math.pow(cube.side, 3))
+
+// console.log(cube)
+
+
+// ======= //
+// 9-mashq //
+// ======= //
+
+
+// const user = {
+//     username: "test",
+//     password: '12345678'
+// }
+
+// if (user.password.length >= 8) {
+//     user.isStrong = true;
+// } else {
+//     user.isStrong = false
+// }
+
+// console.log (user)
+
+
+// ======== //
+// 10-mashq //
+// ======== //
+
+// const person = {
+//     name: "laylo",
+//     age: 20
+// }
+
+// person.birthyear = 2025 - person.age
+
+// console.log(person)
