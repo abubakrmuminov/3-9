704aa54c4d54b2b6e709d0a92131996693225acb
Abubakr <170409v@gmail.com>
2025-04-17 22:07:04 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,129 @@
+// // ============================================================ //
+// // ========================== 1-QISM ========================== //
+// // ============================================================ //
+
+// // ======= //
+// // 1-mashq //
+// // ======= //
+
+// function getUserInfo(user) {
+//   return `Foydalanuvchi ismi: ${user.name}, yoshi: ${user.age}`;
+// }
+
+// const user = { name: "Ali", age: 25 };
+// console.log(getUserInfo(user));
+
+// // ======= //
+// // 2-mashq //
+// // ======= //
+
+// function addTax(product) {
+//   const tax = product.price * 0.12;
+//   product.priceWithTax = product.price + tax;
+//   return product;
+// }
+
+// const product = { name: "Telefon", price: 1000 };
+// console.log(addTax(product));
+
+// // ======= //
+// // 3-mashq //
+// // ======= //
+
+// function carInfo(car) {
+//   return `Bu ${car.color} rangli ${car.model}, ${car.year}-yil`;
+// }
+
+// const car = { model: "Cobalt", year: 2022, color: "oq" };
+// console.log(carInfo(car));
+
+// // ======= //
+// // 4-mashq //
+// // ======= //
+
+// function hasEmail(user) {
+//   if (user.email) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// const user1 = { name: "Ali", email: "ali@gmail.com" };
+
+// console.log(hasEmail(user1));
+
+// // ======= //
+// // 5-mashq //
+// // ======= //
+
+// // ======= //
+// // 6-mashq //
+// // ======= //
+
+// function calculateSpeed(car) {
+//   car.speed = car.distance / car.time;
+//   return car;
+// }
+
+// const myCar = { model: "Cobalt", distance: 150, time: 3 };
+// console.log(calculateSpeed(myCar));
+
+// // ======= //
+// // 7-mashq //
+// // ======= //
+
+// function calculateWorkTime(worker) {
+//   worker.hoursWorked = worker.end - worker.start;
+//   return worker;
+// }
+
+// const ali = { name: "Ali", start: 9, end: 18 };
+// console.log(calculateWorkTime(ali));
+
+// // ======= //
+// // 8-mashq //
+// // ======= //
+
+// function calculateVolume(cube) {
+//   cube.volume = Math.pow(cube.side, 3);
+//   return cube;
+// }
+
+// const result = calculateVolume({ side: 4 });
+// console.log(result);
+
+// // ======= //
+// // 9-mashq //
+// // ======= //
+
+// function isPasswordStrong(user) {
+//   user.isStrong = user.password.length >= 8;
+//   return user;
+// }
+
+// const result = isPasswordStrong({ username: "test", password: "12345678" });
+// console.log(result);
+
+// // ======== //
+// // 10-mashq //
+// // ======== //
+
+// function calculateBirthYear(person) {
+//   const currentYear = 2025;
+//   person.birthYear = currentYear - person.age;
+//   return person;
+// }
+
+// const result = calculateBirthYear({ name: "Laylo", age: 20 });
+// console.log(result);
+
+
+// // ============================================================ //
+// // ========================== 2-QISM ========================== //
+// // ============================================================ //
+
+// // ======= //
+// // 1-mashq //
+// // ======= //
+
